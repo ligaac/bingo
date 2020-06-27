61c5d3caff772cd936f988e5a18ce0890cebc97b
Arpad Gabor <hello@arpadgabor.com>
2020-06-27 20:43:42 +0300

    added bingo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Bingo</h1>
+    <section class="bingo-grid">
+      <bingo-item 
+        v-for="item of bingo"
+        :key="item.id"
+        :id="item.id"
+        :label="item.label"
+        :marked="item.marked"
+        @mark="mark(item.id)"
+      ></bingo-item>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BingoItem from './components/BingoItem'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BingoItem
+  },
+  data() {
+    return {
+      bingo: [
+        { label: 'la multi ani liga ac', id: 1, marked: false },
+        { label: 'Nopți nedormite', id: 2, marked: false },
+        { label: 'Upara', id: 3, marked: false },
+        { label: 'Cineva se emoționează', id: 4, marked: false },
+        { label: 'Ședințe până dimineața', id: 5, marked: false },
+        { label: 'liga ac este o familie', id: 6, marked: false },
+        { label: 'nu m-am gândit ce sa zic', id: 7, marked: false },
+        { label: 'să ne vedem la un ceai', id: 8, marked: false },
+        { label: 'la multi ani liga ac', id: 9, marked: false },
+      ]
+    }
+  },
+  methods: {
+    mark(id) {
+      this.bingo.forEach(item => {
+        if(item.id === id) item.marked = true
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.bingo-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 1em;
 }
 </style>
---
+++ src/components/BingoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <button type="button" @click="$emit('mark', slug)" :disabled="marked">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['label', 'slug', 'marked'],
+  methods: {
+    mark(slug) {
+      this.$emit('mark', slug)
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  height: 10em;
+}
+</style>
